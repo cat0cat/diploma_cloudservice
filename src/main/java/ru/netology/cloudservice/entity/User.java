58d743582927fc14792a45038cb74546ba462c36
Natalia Zubareva <n.zubareva@inbox.ru>
2022-08-20 13:34:56 +0300

    fixed tables

--- src/main/java/ru/netology/cloudservice/entity/User.java
+++ src/main/java/ru/netology/cloudservice/entity/User.java
@@ -1,6 +1,7 @@
 package ru.netology.cloudservice.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Data
 @Entity
+@Builder
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
